404edbad032dbe69d37384c8f798ff901372ad9b
Roman <roman.spam2004@yaandex.ru>
2024-02-09 12:49:06 +0300

    Initial Commit

---
+++ Laba1_4sem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Laba1_4sem
+//
+//  Created by Роман Перепонов on 09.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
